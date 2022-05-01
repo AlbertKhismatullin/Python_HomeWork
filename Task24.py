73c5287330d7d396983d305b8340515fa44226b6
Albert <alberthismatullin02795@gmail.com>
2022-05-01 17:42:25 +0500

    add Task24

--- Task24.py
+++ Task24.py
@@ -1,3 +1,20 @@
 # Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между 
 # максимальным и минимальным значением дробной части элементов.
 
+from random import uniform  #(переменная) равномерная: (a: float, b: float) -> float. 
+#Получите случайное число в диапазоне [a, b) или [a, b] в зависимости от округления.
+n = 5
+frst = 1
+last = 10
+def get_real_nums (n, frst, last): #((функция) get_real_nums: (n: Любой, первый: Любой, последний: Любой) -> список[с плавающей точкой]
+    return [round(uniform(frst,last), 2) for i in range(n)]
+
+def find_diff(my_list):
+    nums = [round(x - int(x), 2) for x in (my_list)]
+    return max(nums) - min(nums)
+
+
+my_list = get_real_nums(n, frst, last)
+
+print (my_list)
+print(find_diff(my_list))
