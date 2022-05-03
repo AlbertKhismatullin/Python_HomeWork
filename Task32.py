4eb492c702f864e87f70adf9e9c5b693d7a1b891
Albert <alberthismatullin02795@gmail.com>
2022-05-03 18:21:14 +0500

    add Task32

---
+++ Task32.py
@@ -0,0 +1,22 @@
+#Дана последовательность чисел. Получить список неповторяющихся элементов исходной последовательности
+
+import numbers
+
+
+numbers = [1, 2, 3, 5, 1, 5, 3, 10] 
+
+# def get_unique_numbers(numbers):
+#     list_of_unique_numbers = [] #пустой список, который будет включать все уникальные числа
+#     unique_numbers = set(numbers) #используется set для получения уникальных чисел из списка numbers
+
+#     for number in unique_numbers: #В итоге имеется перечень из уникальных чисел. Осталось сделать из него список. 
+#                                   #Для этого можно использовать цикл, перебирая каждый из элементов.
+#         list_of_unique_numbers.append(number) #На каждой итерации текущее число добавляется в список
+
+#     return list_of_unique_numbers #именно этот список возвращается в конце программы.
+
+# print(get_unique_numbers(numbers))
+
+#короткий вариант кода с set:
+unique_numbers = list(set(numbers))
+print(unique_numbers)
